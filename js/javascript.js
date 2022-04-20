5a46ea5a9031ce838f0a5c4bb067035e9a36294e
BenSchneider1101 <95427882+BenSchneider1101@users.noreply.github.com>
2022-04-20 12:06:02 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -55,25 +55,13 @@ function polygonStyle(feature) {
 }
 
 
-// add geoJSON points layer and style the points
-function createCircle(feature, latlng) {
- let options = {
- radius: 5,
- fillColor: 'red',
- fillOpacity: 0.5,
- color: 'red',
- weight: 1,
- opacity: 1,
- }
- return L.circleMarker(latlng, options)
-}
-
+// add geoJSON layer
 async function addCelltowersGeoJson(url) {
  const response = await fetch(url)
  const data = await response.json()
- const circles = L.geoJson(data, {
- pointToLayer: createCircle,
- })
- circles.addTo(map)
+ const markers = L.geoJson(data) 
+ const clusters = L.markerClusterGroup() 
+ clusters.addLayer(markers) 
+ clusters.addTo(map) 
 }
 addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
